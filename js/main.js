85bdc7862ce3d3d1aa32d401a0d0af6916900f77
yuki11 <yukipo.kobayakawa@gmail.com>
2021-06-28 14:13:10 -0300

    primeiros codigos

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let nome = "Yuki";
+let idade = 24;
+let idade2 = 10;
+
+let frase = "Eu dá";
+
+//alert(nome + " tem " + idade);
+//alert("mau primeio js");
+
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+
+//troca a palavras
+console.log(frase.replace("Eu", "Me"));
+
+console.log(frase.toUpperCase());
+alert(frase.toLowerCase());
