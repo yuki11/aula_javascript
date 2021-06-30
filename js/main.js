d32839df2db30a80ecc19894023cdb83eba396cd
yuki11 <yukipo.kobayakawa@gmail.com>
2021-06-30 15:30:33 -0300

    aulajs2
    aula 2 js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let nome = "Yuki";
+//aula1
+/*let nome = "Yuki";
 let idade = 24;
 let idade2 = 10;
 
@@ -16,4 +17,52 @@ console.log(idade + idade2);
 console.log(frase.replace("Eu", "Me"));
 
 console.log(frase.toUpperCase());
-alert(frase.toLowerCase());
+alert(frase.toLowerCase());*/
+
+//aula2
+/*
+let lista = ["maça", "pera","laranja"];
+
+//adicionar no ultimo
+lista.push("uva");
+
+
+console.log(lista);
+console.log(lista[0]);
+alert(lista[2]);
+
+//tirar ultima
+lista.pop();
+console.log(lista);
+
+//tamanho
+console.log(lista.length);
+
+//inverte a ordem da lista
+console.log(lista.reverse);
+
+//transforma em de lista para string
+console.log(lista.toString());
+
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+
+console.log(lista);
+console.log(lista.toString());
+
+console.log(lista.join(" "));
+console.log(lista.join(" / "));
+console.log(lista.join(" , "));
+*/
+
+//json/dicionario e lista de dicionarios
+/*
+let fruta = {nome: "maça", cor: "vermelho"}
+
+console.log(fruta.nome);
+alert(fruta.cor);
+
+let frutas = [{nome: "maça", cor: "vermelho"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas.nome);
+console.log(frutas[0].nome);
+*/
